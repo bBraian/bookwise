bbafa0eb3edfed9ccbdb10c3a101ac4a79cd3c88
bBraian <braianvoficial@gmail.com>
2024-11-04 21:12:21 -0300

    feat: criando as views

---
+++ dados.php
@@ -0,0 +1,16 @@
+<?php
+
+$livros = [
+  ['id' => 1, 'titulo' => 'Senhor dos Anéis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A história é ambientada na Terra Média onde senhores buscavam por anéis malignos cujo poder poderia transformá-los em algo muito ruim'],
+  ['id' => 2, 'titulo' => 'O Hobbit', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A jornada de Bilbo Bolseiro em busca de tesouros com um grupo de anões para derrotar o dragão Smaug'],
+  ['id' => 3, 'titulo' => 'A Guerra dos Tronos', 'autor' => 'George R.R. Martin', 'descricao' => 'Vários reinos disputam o Trono de Ferro, com intrigas e batalhas épicas'],
+  ['id' => 4, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'Uma distopia onde o governo totalitário controla todos os aspectos da vida, incluindo pensamentos e ações'],
+  ['id' => 5, 'titulo' => 'Harry Potter e a Pedra Filosofal', 'autor' => 'J.K. Rowling', 'descricao' => 'Um jovem bruxo descobre seu destino ao se matricular em Hogwarts e enfrentar forças sombrias'],
+  ['id' => 6, 'titulo' => 'O Código Da Vinci', 'autor' => 'Dan Brown', 'descricao' => 'Um professor de simbologia tenta resolver um mistério que pode abalar as fundações da Igreja Católica'],
+  ['id' => 7, 'titulo' => 'O Nome do Vento', 'autor' => 'Patrick Rothfuss', 'descricao' => 'A vida de um jovem que se torna uma lenda ao aprender magia e enfrentar seus demônios pessoais'],
+  ['id' => 8, 'titulo' => 'Duna', 'autor' => 'Frank Herbert', 'descricao' => 'Em um futuro distante, uma luta pelo controle do planeta desértico Arrakis e seu recurso mais valioso, a especiaria Melange'],
+  ['id' => 9, 'titulo' => 'A Revolução dos Bichos', 'autor' => 'George Orwell', 'descricao' => 'Uma fábula sobre animais que se rebelam contra seus donos humanos, espelhando os perigos de regimes autoritários'],
+  ['id' => 10, 'titulo' => 'A Menina que Roubava Livros', 'autor' => 'Markus Zusak', 'descricao' => 'A história de uma jovem na Alemanha nazista que encontra consolo nos livros enquanto o mundo ao seu redor desmorona']
+];
+
+?>
--- livro.php
+++ livro.php
@@ -1,30 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Document</title>
-</head>
-<body class="bg-stone-950 text-stone-200">
+<?php
 
-  <header class="bg-stone-900">
-    <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
-      <div class="font-bold text-xl tracking-wide">Book Wise</div>
-      <ul class="flex space-x-4 font-bold">
-        <li><a href="/" class="text-lime-500 tracking-wide">Explorar</a></li>
-        <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
-      </ul>
-      <ul>
-        <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
-      </ul>
-    </nav>
-  </header>
+require 'dados.php';
 
-  <main class="mx-auto max-w-screen-lg space-y-6">
-   
-    
-  </main>
-  
-</body>
-</html>
+$id = $_REQUEST['id'];
+$filtrado = array_filter($livros, fn($l) => $l['id'] == $id);
+$livro = array_pop($filtrado);
+
+$view = "livro";
+
+require 'views/template/app.php';
+
+?>
